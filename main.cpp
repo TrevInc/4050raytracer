b0e0c3f5df4bd967657b5e10a17771cd02e58d9a
jonathan29690 <jonathan29690@live.com>
2014-04-22 23:25:19 -0400

    tokenizer

--- main.cpp
+++ main.cpp
@@ -13,15 +13,7 @@ int main() {
     rayTracer.loadShape("models/teapot.obj");
     rayTracer.ambientLight(Color(0.1,0.1,0.1));
     rayTracer.enableReflections();
-    //rayTracer.enableReflections();
-    //rayTracer.enableAntialiasing();
-    //rayTracer.antialiasingLevel(12);
-    //rayTracer.addLight(new SpotLight("SpotLight1", Color(1, 1, 1), Vector(0, -50, 43), Vector(0, 1, -0.5), 1, 20, 30));
-   // rayTracer.addLight(new PointLight("PointLight1", Vector(0,-40, 40), Color(1, 1, 1), 1));
-    //rayTracer.addLight(new PointLight("PointLight1", Vector(40,0,0), Color(1, 1, 1), 0.8));
     rayTracer.addLight(new ParallelLight("PointLight1", Color(1, 1, 1), Vector(0,0,0), Vector(1,-1,-1), 0.6));
-    //rayTracer.addLight(new ParallelLight("PointLight1", Color(1, 1, 1), Vector(0,0,0), Vector(0,0,-1), 0.5));
-    //rayTracer.addLight(new ParallelLight("PointLight1", Color(1, 1, 1), Vector(0,0,0), Vector(1,1,0), 0.4));
     rayTracer.render();
     rayTracer.saveImage("images/out.png");
     return 0;
